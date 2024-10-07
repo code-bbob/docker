f6478c07de3f6d3470c1eae4461b321cd38258ae
Bibhab Basnet <bbobbasnet@gmail.com>
2024-10-07 10:46:34 +0545

    urls modified

--- backend/transactions/urls.py
+++ backend/transactions/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.TransactionView.as_view(), name='transactions'),
     path('search',views.SearchTransactionView.as_view(), name="search"),
     path('credit/',views.CreditView.as_view(), name="credit"),
+    path('credit/<int:pk>',views.CreditView.as_view(), name="credit"),
     path('credittransactions/',views.CreditTransactionView.as_view(),name = "credit_transactions")
 ]
